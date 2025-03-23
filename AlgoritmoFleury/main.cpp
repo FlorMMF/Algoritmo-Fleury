9df9f755804ad2833253fb51de0fae7721ca6da4
verdielias <centellaperegrina@gmail.com>
2025-03-23 01:44:41 -0700

    Menú de usuario
    El usuario inserta cuantos nodos quiere, así como sus nombres para después elegir el número de aristas y entre que nodos pasa, todo para al final hacer la comprobación de si hay algún paseo de Euler y realizar el algoritmo de Fleury

--- AlgoritmoFleury/main.cpp
+++ AlgoritmoFleury/main.cpp
@@ -1,59 +1,57 @@
 #include <iostream>
-
 #include "Grafica.hpp"
 using namespace std;
 
-int main()
-{
+bool esEuler(const Grafica &g);
+
+int main(){
     try{
         Grafica g;
-        //ejemplo para las pruebas
-        /*
+        int numNod, numAris;
+        
+        cout << "Ingrese al n\243mero de nodos: ";
+        cin >> numNod;
+        
+        for (int i = 0; i < numNod; ++i){
+            char nodo;
+            cout << "Ingresa el nombre del nodo " << i + 1 << ": ";
+            cin >> nodo;
+            g.Agregar(nodo);
+        }
+        
+        cout << "Ingresa el n\243mero de aristas: ";
+        cin >> numAris;
+        
+        for (int i = 0; i < numAris; ++i){
+            char inicial, final;
+            cout << "Ingresa la arista: " << i + 1 << " entre dos nodos separados por un espacio: ";
+            cin >> inicial >> final;
+            g.Agregar(inicial, final);
+        }
+        
+        cout << "La gr\240fica ingresada es: \n";
+        g.Imprimir();
+        
+        if (esEuler(g)){
+            cout << "La gr\240fica es Eulariana, por lo que se ejecutar\240 el algoritmo de Fleury. \n";
+            cout << "Paseo Eulariano: " << AlgoritmoFleury(g) << endl;
+        } else{
+            cout << "La gr\240fica no es Elariano, por lo que el algoritmo de Fleury es imposible de ejecutarse. \n";
+        }
 
 
-        g.Agregar('a');
-        g.Agregar('b');
-        g.Agregar('c');
-        g.Agregar('d');
-        g.Agregar('e');
-        g.Agregar('f');
-
-        g.Agregar('a', 'b');
-        g.Agregar('a', 'd');
-        g.Agregar('a', 'e');
-        g.Agregar('a', 'f');
-        g.Agregar('b', 'f');
-        g.Agregar('b', 'd');
-        g.Agregar('b', 'c');
-        g.Agregar('c', 'd');
-        g.Agregar('d', 'e');
-
-        */
-        g.Agregar('a');
-        g.Agregar('b');
-        g.Agregar('c');
-        g.Agregar('d');
-        g.Agregar('e');
-        g.Agregar('f');
-        g.Agregar('g');
-
-        g.Agregar('a','f');
-        g.Agregar('a','b');
-        g.Agregar('b','f');
-        g.Agregar('b','c');
-        g.Agregar('b','g');
-        g.Agregar('c','d');
-        g.Agregar('c','e');
-        g.Agregar('c','g');
-        g.Agregar('d','e');
-        g.Agregar('g','e');
-        g.Agregar('g','f');
-        g.Agregar('e','f');
-
-        cout << endl << AlgoritmoFleury(g) << endl;
     }catch(const char * mensaje){
-        cerr << mensaje;
+        cerr << mensaje << endl;
     }
 
     return 0;
+}
+
+bool esEuler(const Grafica &g){
+    for (char i = 'a'; i <= 'z'; ++i){ //lo puse como char pero puede ser cualquier cosa
+        if (g.Buscar(i) && g.ObtenerGrado(i) % 2 != 0){
+            return false;
+        }
+    }
+    return true;
 }
